36c68f569831ad1bbc52f9baa4f0e5dd19261efc
Uriel García Luna Bobadilla <uriel_06@ciencias.unam.mx>
2020-05-03 15:30:40 -0500

    Turnos terminados

--- src/js/game.jsx
+++ src/js/game.jsx
@@ -3,40 +3,39 @@ import imgs from './imgs/garcia'
 import Cards from './cards'
 
 
-
-
-function flipCard(e){
-    let card = e.target.parentElement;
-    card.style.transform = "rotateY(180deg)";
-}
-
 function checkCards(card1,card2){
     if (card1.id === card2.id) {
-        setTimeout(() => {
-            card1.classList.add("hide");
-            card2.classList.add("hide");
-        }, 5000);
+        card1.classList.add("hide");
+        card2.classList.add("hide");
     }else{
         setTimeout(() => {
             card1.style.transform = "rotateY(360deg)";
             card2.style.transform = "rotateY(360deg)"; 
-        }, 5000);
-
+        }, 2000);
     }
 }
 
 const Game = () =>{
-    const [card,setCard] = useState(null);
+    const [firstCard,setCard] = useState(null);
+    const [turnOver,setTurn] = useState(true);
     return(
           <Cards imgs={imgs} 
           onClick={(e)=>{
-            if (!card) {
-                flipCard(e);
-                setCard(e.target.parentElement);    
+            const card = e.target.parentElement;
+            if(!card.classList.contains("card") || card.classList.contains("hide") || !turnOver){
+                return;
+            }
+            if (!firstCard) {
+                card.style.transform="rotateY(180deg)";
+                setCard(card);  
             }else{
-                flipCard(e);
-                checkCards(card,e.target.parentElement);
+                setTurn(false);
+                card.style.transform="rotateY(180deg)";
+                checkCards(card,firstCard);
                 setCard(null);
+                setTimeout(() => {
+                    setTurn(true);
+                },2000);
             }
           }
         }></Cards>
